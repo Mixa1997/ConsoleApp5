23eb5e608929b5e698e0dcc0ee2381320d1faf31
Mixa1997 <43315299+Mixa1997@users.noreply.github.com>
2020-10-09 14:46:37 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+                //Высокий уровень!
+            {
+                Console.Write("Введите x1:");
+                double x = double.Parse(Console.ReadLine());
+                Console.Write("Введите y1:");
+                double y = double.Parse(Console.ReadLine());
+                if (y >= x && y >= -x && y <= 1 && y >= -2 + 0) Console.WriteLine("Попадает");
+                else Console.WriteLine("Не попадет");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
